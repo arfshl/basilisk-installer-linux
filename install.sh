977d88085ed6fc8f008a56fa568c9c65adcdb660
arfshl <98870352+arfshl@users.noreply.github.com>
2025-03-15 18:14:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo wget https://dl.basilisk-browser.org/basilisk-20250220145130.linux-x86_64-g
 cd /opt
 sudo tar xvf /opt/basilisk*.tar.*
 sudo chmod -R 755 /opt/basilisk*
-ln -s /opt/basilisk/basilisk /usr/local/bin/basilisk
+sudo ln -s /opt/basilisk/basilisk /usr/local/bin/basilisk
 sudo wget https://github.com/arfshl/basilisk-installer-linux/raw/main/basilisk.desktop -P /usr/local/share/applications
 rm /opt/basilisk*.tar.* install.sh
 basilisk
